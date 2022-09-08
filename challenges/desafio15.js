69a8a5ca7ad1a25bfb98c9b1c964d7aa6817bf19
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-09-08 19:50:02 -0300

    feat: :white_check_mark: Requisito 15 feito.
    Uma query que adiciona o campo avaliacao do tipo NumberInt em todos os documentos e inicia o campo com o número 0, outra que altera todos as avaliações que tenham a tag bovino para 5, e outra que altera todos que tenham a tag ave para 3.

---
+++ challenges/desafio15.js
@@ -0,0 +1,4 @@
+db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
+db.produtos.updateMany({ tags: { $in: [/bovino/i] } }, { $set: { avaliacao: NumberInt(5) } });
+db.produtos.updateMany({ tags: { $in: [/ave/i] } }, { $set: { avaliacao: NumberInt(3) } });
+db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
